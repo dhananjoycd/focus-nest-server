fa4d566169602ce04580669589d9895de83f0ffb
dhananjoycd <dcdmf2020@gmail.com>
2025-07-05 02:48:51 +0600

    Finally JWT update

--- src/routes/users.js
+++ src/routes/users.js
@@ -9,14 +9,13 @@ const {
   fbCuri,
   registerUser1,
 } = require("../controllers/userController");
-const { verifyToken } = require("../middleware/jwt");
 const router = express.Router();
 
 router.post("/fb", registerUser1);
 router.post("/signUp", registerUser);
-router.get("/fbCur", verifyToken, getAllUsers1);
-router.get("/", verifyToken, getAllUsers);
-router.get("/:id", verifyToken, getUser);
-router.put("/:id", verifyToken, updateUserById);
-router.delete("/:id", verifyToken, deleteUserById);
+router.get("/fbCur", getAllUsers1);
+router.get("/", getAllUsers);
+router.get("/:id", getUser);
+router.put("/:id", updateUserById);
+router.delete("/:id", deleteUserById);
 module.exports = router;
